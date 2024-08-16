6a0e8017f7fcbdd28f996eda781ad7b64a53b967
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-16 22:34:25 +0700

    async coroutines

--- app/src/main/java/com/example/coroutinesapp/SecondActivity.kt
+++ app/src/main/java/com/example/coroutinesapp/SecondActivity.kt
@@ -2,10 +2,7 @@ package com.example.coroutinesapp
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.coroutinesapp.databinding.ActivitySecondBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,13 +23,16 @@ class SecondActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.Default).launch {
             Log.d("TAGY1", "The App Started")
 
-            val firstNumber = async { generateFirstNumber() }
-            val secondNumber = async { generateSecondNumber() }
-            val result = firstNumber.await() + secondNumber.await()
+            val firstDeferred = async { generateFirstNumber() }
+            val secondDeferred = async { generateSecondNumber() }
+
+            val firstNumber = firstDeferred.await() // Lưu kết quả vào biến
+            val secondNumber = secondDeferred.await()
+            val result = firstNumber + secondNumber
 
             withContext(Dispatchers.Main) {
-                binding.firstNumberText.text = firstNumber.await().toString()
-                binding.secondNumberText.text = secondNumber.await().toString()
+                binding.firstNumberText.text = firstNumber.toString() // Sử dụng biến đã lưu
+                binding.secondNumberText.text = secondNumber.toString()
                 binding.resultNumberText.text = result.toString()
             }
         }
